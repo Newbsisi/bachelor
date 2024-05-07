98847bd65836ec4cd3eb72a143c679007e77738b
SimonGodthaab <simon.godthaaaab@gmail.com>
2024-05-07 14:29:53 +0200

    changed server.c

--- sphincs/ref/server.c
+++ sphincs/ref/server.c
@@ -59,7 +59,7 @@ int main() {
             printf("Data received: %d bytes\n", read_size);
 
             // Verify the signature and extract the message
-            if (crypto_sign_open(client_message, &message_len, client_message, read_size, public_key) != 0) {
+            if (crypto_sign_open(client_message, &message_len, client_message, read_size, public_key) == 0) {
                 printf("Signature verification failed\n");
             } else {
                 printf("Received and verified message: %.*s\n", (int)message_len, client_message);
