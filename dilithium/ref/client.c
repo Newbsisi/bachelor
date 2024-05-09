d0f95fa2dc5c341195ddfa5616d8e4ca36142495
SimonGodthaab <simon.godthaaaab@gmail.com>
2024-05-09 10:56:03 +0200

    changed client.c and server.c

--- dilithium/ref/client.c
+++ dilithium/ref/client.c
@@ -18,55 +18,66 @@ int main() {
     int sockfd;
     struct sockaddr_in servaddr;
 
-    // Initialize socket
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         error("Socket creation failed");
 
     memset(&servaddr, 0, sizeof(servaddr));
-
-    // Set server address
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); // Change to the IP address of the target Raspberry Pi
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servaddr.sin_port = htons(PORT);
 
-    // Connect to server
     if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
         error("Connection to server failed");
 
-    // Generate key pair
     uint8_t pk[pqcrystals_dilithium5_ref_PUBLICKEYBYTES];
     uint8_t sk[pqcrystals_dilithium5_ref_SECRETKEYBYTES];
-
-    // Generate key pair
     if (pqcrystals_dilithium5_ref_keypair(pk, sk) != 0) {
         fprintf(stderr, "Failed to generate key pair\n");
         return 1;
     }
-    // Send the public key to the server
+
     if (send(sockfd, pk, sizeof(pk), 0) < 0) {
-        perror("Send failed");
+        perror("Send public key failed");
+        return 1;
+    }
+    printf("Public key sent.\n");
+
+    const char *message = "Hello, Raspberry Pi!";
+    size_t message_len = strlen(message);
+
+    uint8_t signature[pqcrystals_dilithium5_ref_BYTES];
+    size_t signature_len;
+    if (pqcrystals_dilithium5_ref_signature(signature, &signature_len, (const uint8_t *)message, message_len, sk) != 0) {
+        fprintf(stderr, "Failed to sign message\n");
         return 1;
     }
 
-    // Message
-    const char *message = "Hello, Raspberry Pi!";
+    size_t packet_len = message_len + signature_len;
+    uint8_t *packet = malloc(packet_len);
+    if (packet == NULL) {
+        fprintf(stderr, "Failed to allocate memory for packet\n");
+        return 1;
+    }
+    memcpy(packet, message, message_len);
+    memcpy(packet + message_len, signature, signature_len);
 
-    // Sign message
-    uint8_t signature[pqcrystals_dilithium5_ref_BYTES];
-    size_t signature_len;
-    pqcrystals_dilithium5_ref_signature(signature, &signature_len, (const uint8_t *)message, strlen(message), sk);
+    printf("Sending message and signature\n");
+    uint32_t net_packet_len = htonl(packet_len);
+    if (send(sockfd, &net_packet_len, sizeof(net_packet_len), 0) < 0) {
+        perror("Send packet length failed");
+        free(packet);
+        return 1;
+    }
 
-    // Construct packet to send (message + signature)
-    size_t packet_len = strlen(message) + signature_len;
-    uint8_t packet[packet_len];
-    memcpy(packet, message, strlen(message));
-    memcpy(packet + strlen(message), signature, signature_len);
+    if (send(sockfd, packet, packet_len, 0) < 0) {
+        perror("Send packet failed");
+        free(packet);
+        return 1;
+    }
 
-    // Send packet
-    send(sockfd, packet, packet_len, 0);
     printf("Message sent successfully.\n");
 
-    // Close socket
+    free(packet);
     close(sockfd);
 
     return 0;
--- dilithium/ref/server.c
+++ dilithium/ref/server.c
@@ -8,8 +8,7 @@
 #include <arpa/inet.h>
 
 #define PORT 8888
-#define MAX_PACKET_SIZE 10000
-
+#define MAX_PACKET_SIZE 20000000
 
 void error(const char *msg) {
     perror(msg);
@@ -21,71 +20,74 @@ int main() {
     socklen_t clilen;
     struct sockaddr_in servaddr, cliaddr;
 
-    // Create socket
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         error("Socket creation failed");
 
     memset(&servaddr, 0, sizeof(servaddr));
-
-    // Configure server address
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(PORT);
 
-    // Bind socket
     if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
         error("Socket bind failed");
 
-    // Listen
     if (listen(sockfd, 5) < 0)
         error("Socket listen failed");
 
     printf("Server listening on port %d...\n", PORT);
-
     clilen = sizeof(cliaddr);
 
-    // Accept connection from client
     if ((newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen)) < 0)
         error("Socket accept failed");
-
     printf("Client connected\n");
 
-    // Receive public key from client
     uint8_t pk[pqcrystals_dilithium5_ref_PUBLICKEYBYTES];
     if (recv(newsockfd, pk, sizeof(pk), 0) < 0) {
-        perror("Receive failed");
+        perror("Receive public key failed");
+        return 1;
+    }
+    printf("Public key received.\n");
+
+    uint32_t packet_len;
+    if (recv(newsockfd, &packet_len, sizeof(packet_len), 0) < 0) {
+        perror("Receive packet length failed");
+        return 1;
+    }
+    packet_len = ntohl(packet_len);
+    printf("Packet length received: %u\n", packet_len);
+
+    if (packet_len == 0 || packet_len > MAX_PACKET_SIZE) {
+        fprintf(stderr, "Invalid packet length: %u\n", packet_len);
         return 1;
     }
 
-    printf("Received public key from client\n");
-
-    // Receive message and signature from client
-    size_t packet_len;
-    if (recv(newsockfd, &packet_len, sizeof(size_t), 0) < 0) {
-        perror("Receive failed");
+    uint8_t *packet = malloc(packet_len);
+    if (packet == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
-
-    uint8_t packet[packet_len];
     if (recv(newsockfd, packet, packet_len, 0) < 0) {
-        perror("Receive failed");
+        perror("Failed to receive packet");
+        free(packet);
         return 1;
     }
 
-    // Verify signature
-    if (pqcrystals_dilithium5_ref_verify(packet + strlen((char *)packet), pqcrystals_dilithium5_ref_BYTES, packet, strlen((char *)packet), pk) != 0) {
+    size_t mlen = packet_len - pqcrystals_dilithium5_ref_BYTES;
+    size_t siglen = pqcrystals_dilithium5_ref_BYTES;
+    const uint8_t *sig = packet + mlen;
+
+    printf("Attempting to verify signature...\n");
+    if (pqcrystals_dilithium5_ref_verify(sig, siglen, packet, mlen, pk) != 0) {
         fprintf(stderr, "Signature verification failed\n");
+        free(packet);
         return 1;
     }
 
     printf("Signature verified successfully\n");
+    printf("Received message: %.*s\n", (int)mlen, packet);
 
-    // Extract and print message
-    printf("Received Message: %s\n", packet);
-
-    // Close sockets
     close(newsockfd);
     close(sockfd);
-
+    free(packet);
     return 0;
 }
