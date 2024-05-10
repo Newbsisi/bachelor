0fb77f49d6be8f3cd3931cbab99ba7c934df27f5
Newbsisi <sorenlau2019@gmail.com>
2024-05-10 12:40:16 +0200

    client added

---
+++ dilithium-java-main/src/test/java/Client.java
@@ -0,0 +1,36 @@
+
+
+import net.thiim.dilithium.interfaces.DilithiumParameterSpec;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.security.Signature;
+
+
+public class Client {
+
+    public void signverify() throws Exception {
+        SecureRandom sr = new SecureRandom();
+        KeyPairGenerator kpg = KeyPairGenerator.getInstance("Dilithium");
+        kpg.initialize(DilithiumParameterSpec.LEVEL2, sr);			
+        KeyPair kp = kpg.generateKeyPair();
+
+        Signature sig = Signature.getInstance("Dilithium");
+        sig.initSign(kp.getPrivate());
+        sig.update("Joy!".getBytes());
+        byte[] signature = sig.sign();
+
+        Signature sigv = Signature.getInstance("Dilithium");
+        sigv.initVerify(kp.getPublic());
+        sigv.update("Joy!".getBytes());
+        boolean b = sig.verify(signature);
+        System.out.println(b);
+        System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+
+        try {
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
